9119c58670982563588084293be16f942f0cba78
Alexander Baquero <78622205+babaquero07@users.noreply.github.com>
2024-11-13 11:42:18 -0500

    Add Docker Compose configuration and initial Express server setup

---
+++ backend/src/app/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+import { config } from "dotenv";
+config();
+
+// import { routes } from "./routes";
+
+const app = express();
+
+const allowedOrigins = ["http://localhost:3000", "http://localhost:8080"];
+const options = {
+  origin: allowedOrigins,
+  credentials: true,
+};
+
+// Middlewares
+app.use(cors(options));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Console logger
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Routes
+// app.use("/api/v1", routes);
+
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}/api/v1`);
+});
+
+server.on("error", (error) => {
+  console.error("Error starting server:", error);
+  process.exit(1);
+});
+
+export default app;
